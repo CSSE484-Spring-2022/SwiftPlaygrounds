c51d21e2f2fcaa7e058af3872d7b7a3c837d4224
David Fisher <fisherds@rose-hulman.edu>
2022-03-14 09:48:14 -0400

    progress towards Closures

--- miniExamples/05_Closures.playground/contents.swift
+++ miniExamples/05_Closures.playground/contents.swift
@@ -1,16 +1,36 @@
 var values = [10, 5, 2, 8, 3, 6, 1, 1000]
 
 // Verbose Closure
-
+var numValuesOver5 = values.reduce(0, {(runningTotal: Int, currentValue: Int) -> Int in
+    return currentValue > 5 ? runningTotal + 1 : runningTotal
+})
+numValuesOver5
 
 
 
 
 // Closure parameter name shorthand and trailing closure
+numValuesOver5 = values.reduce(0) {(runningTotal: Int, currentValue: Int) -> Int in
+    return currentValue > 5 ? runningTotal + 1 : runningTotal
+}
+numValuesOver5
 
 
+numValuesOver5 = values.reduce(0) {(runningTotal, currentValue) in
+    return currentValue > 5 ? runningTotal + 1 : runningTotal
+}
+numValuesOver5
 
 
+// Most common pattern!
+numValuesOver5 = values.reduce(0) {(runningTotal, currentValue) in
+    currentValue > 5 ? runningTotal + 1 : runningTotal
+}
+numValuesOver5
+
+
+// Shorter, but not recommended in this case
+numValuesOver5 = values.reduce(0) { $1 > 5 ? $0 + 1 : $0 }
 
 
 // A Closure using a closure for a variable
